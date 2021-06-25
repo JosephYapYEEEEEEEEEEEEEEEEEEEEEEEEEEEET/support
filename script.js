194628e541b21799dfefe4cd1f80d552c7db7918
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-06-25 16:46:17 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const text = document.getElementById("text");
 const emailtext = document.getElementById("emailtext");
 
 if (localStorage.count == "1") {
-  text.innerHTML = "Just contact me.<br/>Seriously."
+  text.innerHTML = "Just contact me.<br/>Seriously.";
+  emailtext.innerHTML = "CONTACT ME:";
 }
 if (!!localStorage.count == false) {
   localStorage.count = "1"
