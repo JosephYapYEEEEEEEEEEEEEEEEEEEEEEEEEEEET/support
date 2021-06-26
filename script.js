180e273a2049e1c4e40ed9e3e0c5e24882bf47c6
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-06-26 11:56:11 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const emailtext = document.getElementById("emailtext");
 if (localStorage.count == "1") {
   text.innerHTML = "Just contact me.<br/>Seriously.";
   emailtext.innerHTML = "CONTACT ME:";
+} else {
+  text.innerHTML = "If you run into issues, turn the extension off and on in the Preferences Pane in the Safari App.";
+  emailtext.innerHTML = "If that doesn't help email me at"
 }
 if (!!localStorage.count == false) {
   localStorage.count = "1"
